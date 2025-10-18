5a7f98971100a3530ea0924be2b58042e01be2ba
omunyin heman <omunyinheheman@gmail.com>
2025-10-18 14:59:28 +0300

    Add auto superuser creation command with admin credentials

--- contacts/__pycache__/management/create_superuser_if_not_exists.py
+++ contacts/__pycache__/management/create_superuser_if_not_exists.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
-from django.conf import settings
 
 class Command(BaseCommand):
     help = "Creates a superuser if it does not already exist."
@@ -8,8 +7,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         User = get_user_model()
         username = "administrator"
-        email = "admin@example.com"
-        password = "admin123"
+        email = "omunyinheheman@gmail.com"
+        password = "HemanAdmin@2025"
 
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
