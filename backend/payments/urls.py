5d1ad35eda459c0e57a56fe8cc781b20dd28d9bd
omunyin heman <omunyinheheman@gmail.com>
2025-10-27 19:07:15 +0300

    Add create-admin endpoint to payments app

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import MpesaStkPushView, MpesaCallbackView, PaypalWebhookView
-from .views import create_admin
-
+from .views import MpesaStkPushView, MpesaCallbackView, PaypalWebhookView, create_admin
 
 urlpatterns = [
     path('mpesa/stkpush/', MpesaStkPushView.as_view(), name='mpesa_stkpush'),
     path('mpesa/callback/', MpesaCallbackView.as_view(), name='mpesa_callback'),
     path('paypal/webhook/', PaypalWebhookView.as_view(), name='paypal_webhook'),
-    path("create-admin/", create_admin),
+    path('create-admin/', create_admin, name='create_admin'),
 ]
